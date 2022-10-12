2d56aac1f78a98b772b8b48f2456ded874728223
anwind-git <work-git@yandex.ru>
2022-10-12 15:56:19 +0300

    аполните спирально массив 4 на 4 числами от 1 до 16.

--- task_58/Program.cs
+++ task_58/Program.cs
@@ -20,57 +20,31 @@ void FillArray(int[,] number)
     for(int i = 0; i < num; i++)
     {
         if(i < 4)
-        {
             number[0, i] = i + 1;
-        }
         if(i > 3 && i < 5)
-        {
             number[1, 3] = i + 1;
-        }
         if(i > 4 && i < 6)
-        {
             number[2, 3] = i + 1;
-        }
         if(i > 5 && i < 7)
-        {
             number[3, 3] = i + 1;
-        }
         if(i > 6 && i < 8)
-        {
             number[3, 2] = i + 1;
-        }
         if(i > 7 && i < 9)
-        {
             number[3, 1] = i + 1;
-        }
         if(i > 8 && i < 10)
-        {
             number[3, 0] = i + 1;
-        }
         if(i > 9 && i < 11)
-        {
             number[2, 0] = i + 1;
-        }
         if(i > 10 && i < 12)
-        {
             number[1, 0] = i + 1;
-        }
         if(i > 11 && i < 13)
-        {
             number[1, 1] = i + 1;
-        }
         if(i > 12 && i < 14)
-        {
             number[1, 2] = i + 1;
-        }
         if(i > 13 && i < 15)
-        {
             number[2, 2] = i + 1;
-        }
         if(i > 14 && i < 16)
-        {
             number[2, 1] = i + 1;
-        }
     }
 }
 
